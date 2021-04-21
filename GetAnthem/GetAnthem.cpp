6e1c6e6550f74e08146f3ce3e3ee046f14e88b7d
ubelhj <ubelhj@uw.edu>
2021-04-21 15:25:15 -0700

    adds plugin description field

--- GetAnthem/GetAnthem.cpp
+++ GetAnthem/GetAnthem.cpp
@@ -2,7 +2,7 @@
 #include "GetAnthem.h"
 
 
-BAKKESMOD_PLUGIN(GetAnthem, "write a plugin description here", plugin_version, PLUGINTYPE_FREEPLAY)
+BAKKESMOD_PLUGIN(GetAnthem, "Get Anthem", plugin_version, PLUGINTYPE_FREEPLAY)
 
 std::shared_ptr<CVarManagerWrapper> _globalCvarManager;
 
